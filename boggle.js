87fb2741a6eadf75206a162a9eff5ff944759d1e
Peter Rust <peter@cornerstonenw.com>
2020-05-06 07:59:01 -0700

    Initial implementation

---
+++ boggle.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+const _ = require('underscore');
+
+let words = fs.readFileSync('scrabble_words.txt', {encoding: 'utf8'});
+words = words.toLowerCase().split(/\r?\n/);
+words.sort();
+
+let input = '';
+for (let arg of process.argv)
+  if (arg.startsWith('--input='))
+    input = fs.readFileSync(arg.replace('--input=', ''), {encoding: 'utf8'});
+
+const w = 4;
+const h = 4;
+const min_word_length = 3;
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const results = [];
+
+// array of all possible directions
+// as (x, y) pairs
+const directions = [
+  [0, -1], // up
+  [1, -1], // up-right
+  [1, 0], // right
+  [1, 1], // down-right
+  [0, 1], // down
+  [-1, 1], // down-left
+  [-1, 0], // left
+  [-1, -1] // up-left
+];
+
+let rows = [];
+
+// parse input into rows
+if (input) {
+  input = input.split(/\r?\n/);
+  for (let line of input) {
+    let row = line.split(' ');
+    rows.push(row);
+  }
+}
+// randomly populate rows
+else {
+  for (let y = 0; y < h; y++) {
+    let row = [];
+    for (let x = 0; x < w; x++)
+      row.push(_.sample(letters));
+    rows.push(row);
+  }
+}
+
+// start word-searching at each x,y position
+for (let x = 0; x < w; x++) {
+  for (let y = 0; y < h; y++)
+    search(x, y, '', []);
+}
+
+for (let result of results)
+  console.log(result);
+
+function search(x, y, word, word_arr) {
+  word += rows[x][y];
+  word_arr = word_arr.slice(); // clone
+  word_arr.push([x, y]);
+
+  var ix = _.sortedIndex(words, word);
+
+  // if it's a word, add it to the results
+  if (words[ix] == word) {
+    if (word.length >= min_word_length)
+      if (!results.includes(word)) // avoid duplicates
+        results.push(word);
+  }
+
+  // if there are words that start with this combo
+  // keep drilling down (otherwise, give up)
+  if (words[ix].startsWith(word)) {
+    for (let dir of directions) {
+      let new_x = x + dir[0];
+      let new_y = x + dir[1];
+      if (isInBounds(new_x, new_y) && !isUsed(new_x, new_y, word_arr))
+        search(new_x, new_y, word, word_arr);
+    }
+  }
+}
+
+function isInBounds(x, y) {
+  return x >= 0 && y >= 0 && x < w && y < h;
+}
+
+function isUsed(x, y, word_arr) {
+  for (let coords of word_arr)
+    if (x == coords[0] && y == coords[1])
+      return true;
+  return false;
+}
